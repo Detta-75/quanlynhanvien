46d0be982843f714b50dffc5f826042774311c65
Le Tran Cong Dat <lephuonguyen021@gmail.com>
2024-12-17 17:35:48 +0700

    up

---
+++ quanlynhanvienh/src/Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+// abstract Employee gồm : họ tên, mã nhân viên , lương cơ bản, phương thức trừu tượng tính lương
+// lớp con kế thừa NewEmployee gồm: số ngày thử việc, thưởng thử việc, tính lương: lương cơ bản+ (số ngày thử việc *  thưởng thử việc)
+// lớp con kế thừa SeniorEmployee gồm: số năm kinh nghiệm, phụ cấp thâm niên, tính lương: lương cơ bản (số năm kinh nghiệm * phụ cấp thâm niên)
+// các chức năng gồm: hiển thị, thêm mới, cập nhật, xóa, tìm theo mã nhân viên,
+}
